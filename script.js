134cb7ac8aadbd68f37c3ebf60ed4a671437ba44
MukkuChemjong <91582471+MukkuChemjong@users.noreply.github.com>
2025-05-11 00:31:45 -0400

    Add logic for panels and navigation bar

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-// Smooth Scrolling for Navigation
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').slice(1);
-        const targetSection = document.getElementById(targetId);
-        targetSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    });
-});
+//Navigation Bar
+const nav = document.querySelector('#navigation');
+let topOfNav = nav.offsetTop;
 
-// Form Submission Handling
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Thank you for reaching out! I’ll get back to you soon.');
-});
+function fixNav(){
+    if(window.scrollY >= topOfNav){
+        document.body.style.paddingTop = nav.offsetHeight + 'px';
+        document.body.classList.add('fixed-nav');
+    } else{
+        document.body.style.paddingTop = 0;
+        document.body.classList.remove('fixed-nav');
+    }
+}
+
+window.addEventListener('scroll', fixNav);
+
+//Project Panels 
+const panels = document.querySelectorAll('.panel');
+const buttons = document.querySelectorAll('.panel button');
+
+function toggleOpenPanel(){
+    this.classList.toggle('open');
+};
+
+function handleTransition(e){
+    if(!e.propertyName.includes("flex")) return;
+
+    this.classList.toggle('open-active');
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpenPanel));
+panels.forEach(panel => panel.addEventListener('transitionend', handleTransition));
+buttons.forEach(button => button.addEventListener('transitionend', handleTransition));
+
+
+
